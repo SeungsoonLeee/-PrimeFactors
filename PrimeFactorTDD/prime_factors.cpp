655a91821b5830fb5886db90b617345d3a1014f8
lss <lss@email.com>
2024-05-16 17:05:08 +0900

    add test of6

--- PrimeFactorTDD/prime_factors.cpp
+++ PrimeFactorTDD/prime_factors.cpp
@@ -8,8 +8,14 @@ public:
 		vector<int> ret = {};
 		if (num > 1) {
 			if (num == 4) {
+				while (num % 2 == 0) {
+					ret.push_back(2);
+					num /= 2;
+				}
+			}
+			else if (num == 6) {
 				ret.push_back(2);
-				ret.push_back(2);
+				ret.push_back(3);
 			}
 			else {
 				ret.push_back(num);
