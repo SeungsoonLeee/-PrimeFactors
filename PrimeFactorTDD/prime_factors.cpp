c85018a0f9996c096425ca4223bc59f0df324fdd
lss <lss@email.com>
2024-05-17 08:34:21 +0900

    refactor test of12

--- PrimeFactorTDD/prime_factors.cpp
+++ PrimeFactorTDD/prime_factors.cpp
@@ -6,18 +6,10 @@ class PrimeFactor
 public:
 	vector<int> of(int num) {
 		vector<int> ret = {};
-		if (num > 1) {
-			int divisor = 2;
-			if (num == 4 || num == 6 || num == 9 || num == 12) {
-				for (divisor = 2; num > 1; divisor++) {
-					while (num % divisor == 0) {
-						ret.push_back(divisor);
-						num /= divisor;
-					}
-				}
-			}
-			else {
-				ret.push_back(num);
+		for (int divisor = 2; num > 1; divisor++) {
+			while (num % divisor == 0) {
+				ret.push_back(divisor);
+				num /= divisor;
 			}
 		}
 		return ret;
