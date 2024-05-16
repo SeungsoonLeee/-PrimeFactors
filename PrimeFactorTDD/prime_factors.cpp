9e48474e4e3b24d2c2fe0fa36e163aa5eca7f1ed
lss <lss@email.com>
2024-05-16 16:56:09 +0900

    add test of2

--- PrimeFactorTDD/prime_factors.cpp
+++ PrimeFactorTDD/prime_factors.cpp
@@ -4,8 +4,11 @@ using namespace std;
 class PrimeFactor
 {
 public:
-	vector<int> of(int i) {
+	vector<int> of(int num) {
 		vector<int> ret = {};
+		if (num == 2) {
+			ret.push_back(2);
+		}
 		return ret;
 	}
 };
--- Sample-Test1/prime_factors_test.cpp
+++ Sample-Test1/prime_factors_test.cpp
@@ -3,9 +3,16 @@
 #include<vector>
 using namespace std;
 
-TEST(PrimeFactors, PrimTest) {
+TEST(PrimeFactors, Of1) {
 	PrimeFactor pf;
 	vector<int> expected = {};
 
 	EXPECT_EQ(expected, pf.of(1));
+}
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactor pf;
+	vector<int> expected = {2};
+
+	EXPECT_EQ(expected, pf.of(2));
 }
