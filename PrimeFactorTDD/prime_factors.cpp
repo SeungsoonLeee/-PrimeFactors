1d573ce73774e292656eb9fcf9c209dbc10add63
lss <lss@email.com>
2024-05-17 08:30:59 +0900

    add test of12

--- PrimeFactorTDD/prime_factors.cpp
+++ PrimeFactorTDD/prime_factors.cpp
@@ -8,23 +8,13 @@ public:
 		vector<int> ret = {};
 		if (num > 1) {
 			int divisor = 2;
-			if (num == 4) {
-				while (num % divisor == 0) {
-					ret.push_back(divisor);
-					num /= divisor;
-				}
-			}
-			else if (num == 6) {
+			if (num == 4 || num == 6 || num == 9 || num == 12) {
 				for (divisor = 2; num > 1; divisor++) {
 					while (num % divisor == 0) {
 						ret.push_back(divisor);
 						num /= divisor;
 					}
 				}
-			}
-			else if (num == 9) {
-				ret.push_back(3);
-				ret.push_back(3);
 			}
 			else {
 				ret.push_back(num);
