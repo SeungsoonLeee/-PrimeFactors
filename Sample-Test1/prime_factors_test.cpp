2aa25ec6f543a894a65ecfa95d0879d7f13e07aa
lss <lss@email.com>
2024-05-16 16:53:11 +0900

    prototype of primefactor project

--- Sample-Test1/prime_factors_test.cpp
+++ Sample-Test1/prime_factors_test.cpp
@@ -1,12 +1,11 @@
 #include "pch.h"
 #include "../PrimeFactorTDD/prime_factors.cpp"
-
+#include<vector>
 using namespace std;
 
 TEST(PrimeFactors, PrimTest) {
+	PrimeFactor pf;
+	vector<int> expected = {};
 
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
+	EXPECT_EQ(expected, pf.of(1));
 }
